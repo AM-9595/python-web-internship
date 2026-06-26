d7c78fc3527af86a76a9e91e0de9d0095113dc47
AM <aydaevmansur.08@mail.ru>
2026-06-26 23:47:59 +0300

    day_19

--- week_3/day_17/tasks/urls.py
+++ week_3/day_17/tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet, TaskViewSet  
+from .views import ProjectViewSet, TaskViewSet
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet)
